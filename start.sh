295be6f07f959c35a191cb1397436c424f685780
boymyd6 <boymyd6@gmail.com>
2024-09-24 14:27:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/FILMCLAN/MYD.git /MYD
+  git clone https://github.com/boymyd6/MYD.git /MYD
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MYD
