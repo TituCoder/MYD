2ec80538d3b701f43a0bb39b811530dce6d06251
boymyd6 <boymyd6@gmail.com>
2024-10-02 08:42:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,8 +106,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://myd-lvx5.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://myd-lvx5.onrender.com/".format(FQDN, PORT)
+URL = "".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -122,9 +122,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://myd-lvx5.onrender.com/".format(FQDN)
+    URL = "".format(FQDN)
 else:
-    URL = "https://myd-lvx5.onrender.com/".format(FQDN)
+    URL = "".format(FQDN)
 
 # add premium logs channel id
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002001547224'))
