ac2c36dde4e083cd294b0a2115ff83846dd1ba2f
TituCoder <asatkarsarvesh39+t2@gmail.com>
2024-11-20 12:36:24 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -62,7 +62,7 @@ def generate_random_alphanumeric():
   
 def get_shortlink_sync(url):
     try:
-        rget = requests.get(f"https://{STREAM_SITE}/api?api={STREAM_API}&url={url}&alias={generate_random_alphanumeric()}")
+        rget = requests.get(f"https://{STREAM_SITE}api?api={STREAM_API}&url={url}&alias={generate_random_alphanumeric()}")
         rjson = rget.json()
         if rjson["status"] == "success" or rget.status_code == 200:
             return rjson["shortenedUrl"]
